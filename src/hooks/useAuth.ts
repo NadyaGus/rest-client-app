d4d93f87c9923a6bf840f64ba8736a839c7de91d
osulyanov <oleg@sulyanov.com>
2025-04-07 10:11:45 +0700

    feat: add useAuth hook
    that gets auth data from supabase, subscribes to update to know when user signs in or out and provides signOut callback

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,38 @@
+'use client';
+
+import { createClient } from '@/utils/supabase/client';
+import { User } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const supabase = createClient();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+      setLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [supabase.auth]);
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return {
+    user,
+    loading,
+    signOut,
+  };
+}
