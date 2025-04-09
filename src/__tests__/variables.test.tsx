0af34dddda26ff959fde448647c30077addb103e
Murad Davydov <mdavydov.dev@gmail.com>
2025-04-09 21:53:03 +0500

    feat: add variables

---
+++ src/__tests__/variables.test.tsx
@@ -0,0 +1,85 @@
+import VariablesPage from '@/app/variables/page';
+import { VARIABLES_LOCAL_STORAGE_KEY } from '@/constants';
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, beforeEach, test } from 'vitest';
+
+describe('VariablesPage Component', () => {
+  beforeEach(() => {
+    cleanup();
+    localStorage.clear();
+  });
+
+  test('renders the Variables page', () => {
+    render(<VariablesPage />);
+    expect(screen.getByText('Variables')).toBeInTheDocument();
+    expect(screen.getByLabelText('Name *')).toBeInTheDocument();
+    expect(screen.getByLabelText('Value *')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Add variable' })).toBeInTheDocument();
+  });
+
+  test('adds a new variable to the grid and local storage', async () => {
+    render(<VariablesPage />);
+
+    const nameInput = screen.getByLabelText('Name *');
+    const valueInput = screen.getByLabelText('Value *');
+    const addButton = screen.getByRole('button', { name: 'Add variable' });
+
+    fireEvent.change(nameInput, { target: { value: 'testName' } });
+    fireEvent.change(valueInput, { target: { value: 'testValue' } });
+    fireEvent.click(addButton);
+
+    await waitFor(() => {
+      expect(localStorage.getItem(VARIABLES_LOCAL_STORAGE_KEY)).toEqual('[{"name":"testName","value":"testValue"}]');
+      expect(screen.getByText('testName')).toBeInTheDocument();
+      expect(screen.getByText('testValue')).toBeInTheDocument();
+    });
+  });
+
+  test('shows error messages when name or value are empty', async () => {
+    render(<VariablesPage />);
+
+    const nameInput = screen.getByLabelText('Name *');
+    const valueInput = screen.getByLabelText('Value *');
+    const addButton = screen.getByRole('button', { name: 'Add variable' });
+
+    fireEvent.change(nameInput, { target: { value: ' ' } });
+    fireEvent.change(valueInput, { target: { value: ' ' } });
+    fireEvent.click(addButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Name is required field')).toBeInTheDocument();
+      expect(screen.getByText('Value is required field')).toBeInTheDocument();
+    });
+  });
+
+  test('updates an existing variable', async () => {
+    localStorage.setItem(VARIABLES_LOCAL_STORAGE_KEY, '[{"name":"testName","value":"oldValue"}]');
+    render(<VariablesPage />);
+
+    const nameInput = screen.getByLabelText('Name *');
+    const valueInput = screen.getByLabelText('Value *');
+    const addButton = screen.getByRole('button', { name: 'Add variable' });
+
+    fireEvent.change(nameInput, { target: { value: 'testName' } });
+    fireEvent.change(valueInput, { target: { value: 'newValue' } });
+    fireEvent.click(addButton);
+
+    await waitFor(() => {
+      expect(localStorage.getItem(VARIABLES_LOCAL_STORAGE_KEY)).toEqual('[{"name":"testName","value":"newValue"}]');
+      expect(screen.getByText('newValue')).toBeInTheDocument();
+    });
+  });
+
+  test('deletes a variable', async () => {
+    localStorage.setItem(VARIABLES_LOCAL_STORAGE_KEY, '[{"name":"testName","value":"testValue"}]');
+    render(<VariablesPage />);
+
+    const deleteButton = screen.getByLabelText('Delete');
+    fireEvent.click(deleteButton);
+
+    await waitFor(() => {
+      expect(localStorage.getItem(VARIABLES_LOCAL_STORAGE_KEY)).toEqual('[]');
+      expect(screen.queryByText('testName')).not.toBeInTheDocument();
+    });
+  });
+});
--- src/app/variables/page.tsx
+++ src/app/variables/page.tsx
@@ -1,9 +1,120 @@
-import { Typography } from '@mui/material';
+'use client';
+
+import { VARIABLES_LOCAL_STORAGE_KEY } from '@/constants';
+import { Add } from '@mui/icons-material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Button, Paper, TextField, Typography } from '@mui/material';
+import { DataGrid, GridActionsCellItem, GridColDef, GridRowId } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+
+interface Variable {
+  name: string;
+  value: string;
+}
+
+const paginationModel = { page: 0, pageSize: 5 };
 
 export default function VariablesPage() {
+  const [variables, setVariables] = useState<Variable[]>([]);
+  const [nameEmptyError, setNameEmptyError] = useState('');
+  const [valueEmptyError, setValueEmptyError] = useState('');
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Name', flex: 1 },
+    { field: 'value', headerName: 'Value', flex: 1 },
+    {
+      field: 'delete',
+      type: 'actions',
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            key={id}
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  useEffect(() => {
+    setVariables(JSON.parse(localStorage.getItem(VARIABLES_LOCAL_STORAGE_KEY) || '[]'));
+  }, []);
+
+  const handleDeleteClick = (name: GridRowId) => () => {
+    const updatedVariables = variables.filter((variable) => variable.name !== name);
+    setVariables(updatedVariables);
+    localStorage.setItem(VARIABLES_LOCAL_STORAGE_KEY, JSON.stringify(updatedVariables));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const data = new FormData(form);
+    const name = (data.get('name') as string).trim();
+    const value = (data.get('value') as string).trim();
+    let isValid = true;
+
+    if (!name) {
+      setNameEmptyError('Name is required field');
+      isValid = false;
+    }
+    if (!value) {
+      setValueEmptyError('Value is required field');
+      isValid = false;
+    }
+
+    if (!isValid) {
+      return;
+    }
+
+    const existsVariable = variables.findIndex((variable) => variable.name === name);
+    const updatedVariables = [...variables];
+
+    if (existsVariable !== -1) {
+      updatedVariables[existsVariable].value = value;
+    } else {
+      updatedVariables.push({
+        name: name,
+        value: value,
+      });
+    }
+
+    setVariables(updatedVariables);
+    localStorage.setItem(VARIABLES_LOCAL_STORAGE_KEY, JSON.stringify(updatedVariables));
+
+    form.reset();
+  };
+
   return (
-    <Typography variant="h4" component="h1">
-      Variables
-    </Typography>
+    <>
+      <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: 2 }}>
+        <Typography variant="h4" component="h1">
+          Variables
+        </Typography>
+
+        <Box component={'form'} onSubmit={handleSubmit} sx={{ display: 'flex', gap: 2 }}>
+          <TextField name="name" label="Name" variant="outlined" required helperText={nameEmptyError} />
+          <TextField name="value" label="Value" variant="outlined" required helperText={valueEmptyError} />
+          <Button variant="contained" startIcon={<Add />} type="submit">
+            Add variable
+          </Button>
+        </Box>
+
+        <Paper sx={{ width: '60%' }}>
+          <DataGrid
+            rows={variables}
+            columns={columns}
+            getRowId={(row) => row.name}
+            initialState={{ pagination: { paginationModel } }}
+            pageSizeOptions={[5, 10, 25, 50]}
+          />
+        </Paper>
+      </Box>
+    </>
   );
 }
