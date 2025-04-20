49bdc61578957d8fc3c92edd1bd2f16bc294d2c5
osulyanov <oleg@sulyanov.com>
2025-04-20 12:42:50 +0700

    feat: async history page with skeleton

---
+++ src/app/history/history-content.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Box, Container, Typography } from '@mui/material';
+
+export const HistoryContent = () => {
+  return (
+    <Container maxWidth="md">
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, py: 4 }}>
+        <Typography variant="h4" component="h1" align="center">
+          History
+        </Typography>
+        TBD
+      </Box>
+    </Container>
+  );
+};
